301bc99fd87b3a120eb9520d30b8c768ecd49c9e
marzz3lo <marzzelo@gmail.com>
2017-05-09 00:02:55 -0400

    Creación de un proyecto base con OpenCV (Base)

--- app/src/main/java/moonlapse/com/proyectobase/MainActivity.java
+++ app/src/main/java/moonlapse/com/proyectobase/MainActivity.java
@@ -1,29 +1,98 @@
 package moonlapse.com.proyectobase;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.InstallCallbackInterface;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2, LoaderCallbackInterface {
 
     private static final int SOLICITUD_PERMISO_CAMERA = 0;
+
+    private static final String TAG = "Ej OCV (MainActivity)";
+    private CameraBridgeViewBase cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+        cameraView = (CameraBridgeViewBase) findViewById(R.id.vista_camara);
+        cameraView.setCvCameraViewListener(this);
     }
 
     @Override
     protected void onResume() {
+        super.onResume();
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
                 ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, SOLICITUD_PERMISO_CAMERA);
             }
         }
-        super.onResume();
+
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (cameraView != null)
+            cameraView.disableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraView != null)
+            cameraView.disableView();
+    }
+
+    @Override
+    public void onManagerConnected(int status) {
+        switch (status) {
+            case LoaderCallbackInterface.SUCCESS:
+                Log.i(TAG, "OpenCV se cargo correctamente");
+                cameraView.enableView();
+                break;
+            default:
+                Log.e(TAG, "OpenCV no se cargo");
+                Toast.makeText(MainActivity.this, "OpenCV no se cargo", Toast.LENGTH_LONG).show();
+                finish();
+                break;
+        }
+    }
+
+    @Override
+    public void onPackageInstall(int operation, InstallCallbackInterface callback) {
+
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
     }
 }
