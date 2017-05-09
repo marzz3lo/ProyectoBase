7bbcbbedfd59b20542b5152a864a5e971307bfb6
marzz3lo <marzzelo@gmail.com>
2017-05-08 23:46:52 -0400

    Initial commit

---
+++ app/src/main/java/moonlapse/com/proyectobase/MainActivity.java
@@ -0,0 +1,29 @@
+package moonlapse.com.proyectobase;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SOLICITUD_PERMISO_CAMERA = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, SOLICITUD_PERMISO_CAMERA);
+            }
+        }
+        super.onResume();
+    }
+}
