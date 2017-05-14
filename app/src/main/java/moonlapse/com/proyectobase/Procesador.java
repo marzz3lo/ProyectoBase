4f311ccbd5ac624a17113d391597345635f4f5d6
marzz3lo <marzzelo@gmail.com>
2017-05-13 22:42:03 -0400

    Ejercicio: Detección de zonas verdes

--- app/src/main/java/moonlapse/com/proyectobase/Procesador.java
+++ app/src/main/java/moonlapse/com/proyectobase/Procesador.java
@@ -34,8 +34,8 @@ public class Procesador {
         Core.extractChannel(entrada, red, 0);
         Core.extractChannel(entrada, green, 1);
         Core.extractChannel(entrada, blue, 2);
-        Core.max(green, blue, maxGB);
-        Core.subtract(red, maxGB, salida);
+        Core.max(red, blue, maxGB);
+        Core.subtract(green, maxGB, salida);
         return salida;
     }
 }
